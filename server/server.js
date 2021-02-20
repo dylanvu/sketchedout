f4e0d5b6435223dc5fce3b913c169ef38862f2ab
jenferp <77474432+jenferp@users.noreply.github.com>
2021-02-20 03:00:21 -0800

    added creatRoom, joinRoom, and updateBoard functions for server and clients

--- server/server.js
+++ server/server.js
@@ -14,12 +14,48 @@ const io = require('socket.io')(http, {
 const PORT = 2000;
 http.listen(PORT, () => console.log('The server is running'));
 
+let roomIDlist = [];
+let board = "Board Test";
+
 io.on('connection', (socket) => {
     console.log("A user has connected!");
-    socket.on('joinRequest', (roomID) => {
-        console.log(roomID)
-        socket.join(roomID)
-        let board = "Board Test"
-        io.to(roomID).emit('loadBoard', board)
+    socket.emit('connection', null)
+
+    // Function to create a room upon create request
+    socket.on('createRequest', () => {
+        var roomID = generateRoomID(6)
+        roomIDlist.push(roomID);
+        socket.join(roomID);
+        socket.emit('New Game Created', board);
     })
+    
+    // Function to join a room upon join request
+    socket.on('joinRequest', (joinRoomID) => {
+        console.log(joinRoomID)
+        
+        // Check to see if the room exists
+        var roomCheck = false;
+        for (room = 0; room < roomIDlist.length; room++) {
+            if (joinRoomID == roomIDlist[room]) {
+                roomCheck = true
+                break
+            }
+        }
+        // If the room does not exist, send an error message to the client
+        if (roomCheck == false) {
+            socket.emit('joinError', null)
+        } else if (roomCheck == true) {    // If the room does exist, join the room and send the board
+            socket.join(joinRoomID);
+
+            // I wonder if there's an issue with this below. Maybe we should only send to a host or something?
+            socket.to(joinRoomID).emit('uponJoiningload', null) // TODO: change this to "sendBoard" or something 
+            // You don't need to call a socket.on to update the board here because when you emit the 'uponJoiningload', on the client side it should trigger a room-wide update function
+            // which will encompass this newly joined socket anyways.
+        }
+    })
+
+    socket.on('updateBoard', (roomInfo) => {
+        socket.to(roomInfo.roomID).emit('newboard', roomInfo.board)
+        }
+    )
 });
--- src/App.js
+++ src/App.js
@@ -1,32 +1,70 @@
 import logo from './logo.svg';
 import './App.css';
-import socketClient from 'socket.io-client'
+import socketClient from 'socket.io-client';
+import './generateroomID';
 
+// This should be the url of the server
 const ENDPOINT = "http://localhost:2000"
 
 function App() {
   // Keep everything below this
+  let roomIDtoJoin = "JoinROOM" // This is a template value. Later on, use a form to change this input
+
   let socket = socketClient(ENDPOINT);
+
+  let roomInfo = {
+    roomID = "myRoomID",
+    currentBoard = "myCurrentboard"
+  }
+
   socket.on('connection', () => {
-    console.log("I'm in the mainframe.")
+    console.log("I'm in the mainframe.");
   })
 
-  // When the client receives the call to load the board, load the board in CanvasDraw
+    // When the client receives the call to load the board, load the board in CanvasDraw
   socket.on('loadBoard', (board) => {
-    console.log(board)
+    // This should load the board (see canvas demo)
+    console.log(board);
   })
-  
-  // When the client requests to join a room, send the roomID to the server
-  function joinRoom() {
-    console.log("Test")
-    socket.emit('joinRequest', "ROOMCODEHERE")
+
+  socket.on('joinError', () => {
+    console.log("Room not found! Make sure you have a valid room code.");
+  })
+
+  socket.on('uponJoiningload', () => {
+    sendBoard(currentBoard);
+  })
+
+  socket.on('newBoard', (newBoard) => {
+    console.log("newBoardhere")
+  })
+
+  // Create room
+  function createRoom() {
+    console.log("Create Room Button pressed")
+    socket.emit('createRequest', null);
   }
+  
+    // When the client requests to join a room, send a "joinRequest" with the contents of roomID to the server
+  function joinRoom() {
+    console.log("Join Room Button pressed");
+    socket.emit('joinRequest', roomIDtoJoin);
+  }
+
+  // This function should save the current board
+  function sendBoard(currentBoard) {
+    socket.emit('updateBoard', roomInfo)
+  }
+
   // Keep everything above this
 
   return (
     <div className="App">
       <header className="App-header">
         {/* Keep this button below */}
+        <button onClick={createRoom}>
+          Create a room
+        </button>
         <button onClick={joinRoom}>
           Join a room
         </button>
